b8f5dd51c23aa25aaf4d88031ca2a2e34bcf9820
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-07-03 20:35:52 +0600

    dive into node and express js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express=require("express")
-const env=require("dotenv")
+const morgan=require("morgan")
 const app=express()
+// app.use(morgan())
 
-app.get("/test",(req,res)=>{
-        res.send("i am listening")
+app.get("/",(req,res)=>{
+        res.send("this is home page")
 })
-
+app.get("/about",morgan("dev"),(req,res)=>{
+        res.send("this is about page")
+})
+app.get("/test",(req,res)=>{
+    res.send("this is test page")
+})
+app.get("*",(req,res)=>{
+    res.send("404 not found")
+})
 
 const PORT=process.env.PORT||3001
 
