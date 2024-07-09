6ae396aca5b59e767f14befbf21059d8fdf5296f
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-07-09 16:12:32 +0600

    Dive into node & express JS

--- contactProject/app.js
+++ contactProject/app.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const app=express()
 const contactRoute=require("./route")
+const bodyParser=require("body-parser")
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.use("/user",contactRoute)
 
--- contactProject/controller.js
+++ contactProject/controller.js
@@ -1,4 +1,5 @@
 const fs=require("fs")
+
 exports.getAllContact=(req,res)=>{
     fs.readFile("./data.js",(err,data)=>{
         res.writeHead(200,{"content-type":"text/html"})
@@ -14,7 +15,24 @@ exports.getOneContact=(req,res)=>{
     })
 }
 exports.createContact=(req,res)=>{
-    res.send("create contact")
+    const name=req.body.name
+   const id=req.body.id
+   const email=req.body.email
+   const phone=req.body.phone
+   const cData=(name,id,email,phone)
+   fs.writeFile("./createdData.js",cData,(err)=>{
+    if(err){
+        res.writeHead(404,{"content-type":"text/html"})
+        res.write("file create failed")
+        res.end()
+    }
+    else{
+        res.writeHead(200,{"content-type":"text/html"})
+        res.write("file create success")
+        res.end()
+    }
+   })
+
 }
 exports.updateContact=(req,res)=>{
     res.send("update contact")
