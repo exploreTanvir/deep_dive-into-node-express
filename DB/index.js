8e6c829fffca0118d58c1b82a1afbdc7bfad601c
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-07-15 23:07:17 +0600

    Dive into Node & Express JS

---
+++ DB/index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("This Is Home Page")
+})
+
+
+const PORT=3005
+app.listen(PORT,()=>{
+    console.log(`Server is running on the port number ${PORT}`)
+})
