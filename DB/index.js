3d06a18ed388684ba87b2f51bed77ac908e95952
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-07-16 23:17:11 +0600

    Dive into Node & Express JS

--- DB/index.js
+++ DB/index.js
@@ -1,5 +1,29 @@
 const express=require("express")
+const mongoose=require("mongoose")
 const app=express()
+
+//Way-1
+
+// mongoose.connect("mongodb://127.0.0.1:27017/usersDB")
+// .then(()=>console.log("DB is connected"))
+// .catch((err)=>{
+//     console.log(err)
+//     console.log("DB is not connected")
+// })
+
+
+//Way-2
+
+const connectDB=async()=>{
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/usersDB")
+        console.log("DB is connected")
+    } catch (error) {
+        console.log("DB is not connected")
+        console.log(error)
+    }
+}
+
 
 app.get("/",(req,res)=>{
     res.send("This Is Home Page")
@@ -7,6 +31,7 @@ app.get("/",(req,res)=>{
 
 
 const PORT=3005
-app.listen(PORT,()=>{
+app.listen(PORT,async()=>{
     console.log(`Server is running on the port number ${PORT}`)
+    await connectDB()
 })
